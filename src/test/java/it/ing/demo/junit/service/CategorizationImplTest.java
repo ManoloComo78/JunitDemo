fef8d505dc96007606a75a32eb810f0079ae6b6f
IngEurCDaaS01 <manolo.vizzini@ing.com>
2023-06-15 11:34:16 +0200

    First commit

--- src/test/java/it/ing/demo/junit/service/CategorizationImplTest.java
+++ src/test/java/it/ing/demo/junit/service/CategorizationImplTest.java
@@ -11,7 +11,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.testcontainers.shaded.com.google.common.collect.Iterables;
 
 import java.util.ArrayList;
 import java.util.Collections;
